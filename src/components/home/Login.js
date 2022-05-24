788e45380379d6ad0808bf2f9021db3fdaa36bb5
Akanksha0401 <akanksha.2024cs1126@kiet.edu>
2022-05-24 16:53:18 +0530

    - added styles to home page

--- src/components/home/Login.js
+++ src/components/home/Login.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='login-container'>
+            <div className='login'>
+                <div className='login-left'>
+                    <div className='login-image'></div>
+                </div>
+                <div className='login-right'>
+                    <div className='login-form'>
+                        <div className='form-title'>
+                            Login
+                        </div>
+                        <div className='form-group'>
+                            <label>Email Id</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-group'>
+                            <label>Password</label>
+                            <input type='text' />
+                        </div>
+                        <div className='form-group'>
+                            <input type='checkbox' />
+                            <label>Remember Me</label>
+                        </div>
+                        <div className='form-group'>
+                            <input type='submit' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
